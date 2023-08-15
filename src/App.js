f468b52e0fb6b0d9f3edc037cc1040f3999875f0
Abstynent <lukjur94@gmail.com>
2023-08-15 23:04:29 +0100

    add projects component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import NavbarComponent from "./components/Navbar";
 import AboutMeComponent from "./components/AboutMe";
+import ProjectsComponent from "./components/Projects";
 
 function App() {
   return (
-    <main className="bg-white px-10 md:px-20 lg:px-40">
-      <section className="min-h-screen">
+    <main className="">
+      <section className="min-h-screen bg-white px-10 md:px-20 lg:px-40">
         <NavbarComponent />
         <AboutMeComponent />
       </section>
+      <ProjectsComponent />
     </main>
   );
 }

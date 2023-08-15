32514a024c925bfac0e576af1672e6fe73335dad
Abstynent <lukjur94@gmail.com>
2023-08-15 23:05:09 +0100

    styling slightly changed

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -9,12 +9,12 @@ import {
 const AboutMeComponent = () => {
   return (
     <>
-      <div className="text-center p-10">
+      <div className="text-center p-5">
         <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
           Lukasz Jurkiewicz
         </h2>
-        <h3 className="text-2xl py-2 md:text-3xl">Full Stack Web Developer</h3>
-        <p className="text-md py-5 leading-8 text-gray-600 md:text-xl max-w-lg mx-auto">
+        <h3 className="text-2xl py-1 md:text-3xl">Full Stack Web Developer</h3>
+        <p className="text-md py-5 leading-8 text-gray-600 md:text-xl  mx-auto">
           Passionate and driven full-stack developer with a proven track record
           of expertise across a diverse spectrum of technologies. My primary
           objective is to craft extraordinary web applications that provide
@@ -27,14 +27,14 @@ const AboutMeComponent = () => {
         </p>
       </div>
 
-      <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+      <div className="text-5xl flex justify-center gap-16 py-1 text-gray-600">
         <AiFillGithub />
         <AiFillLinkedin />
         <AiFillTwitterCircle />
       </div>
 
-      <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
-        <img src={avatar} layout="fill" objectFit="cover" />
+      <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 my-10 overflow-hidden md:h-96 md:w-96">
+        <img src={avatar} />
       </div>
     </>
   );

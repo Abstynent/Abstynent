1185a44e2820c9ece288b64ba440c239c0de55d8
Abstynent <lukjur94@gmail.com>
2023-08-15 20:38:42 +0100

    navbar created

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { useState } from "react";
+import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
+
+const NavbarComponent = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+    <section className="min-h-screen">
+      <nav className="py-10 mb-12 flex justify-between">
+        <h1 className="text-xl font-burtons">wookash</h1>
+        <ul className="flex items-center">
+          <li>
+            {!darkMode ? (
+              <BsFillMoonStarsFill
+                onClick={() => setDarkMode(!darkMode)}
+                className=" cursor-pointer text-2xl"
+              />
+            ) : (
+              <BsFillSunFill
+                onClick={() => setDarkMode(!darkMode)}
+                className=" cursor-pointer text-2xl"
+              />
+            )}
+          </li>
+          <li>
+            <a href="" className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8">Resume</a>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
+};
+
+export default NavbarComponent;
